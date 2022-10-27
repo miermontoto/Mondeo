788661762874dc7f0ee001aa662c31d2a036193b
miermontoto <mier@mier.info>
2022-10-27 21:14:43 +0200

    fixed bugs, reduced amount of code

--- app/src/main/java/com/mier/mondeo/MainActivity.java
+++ app/src/main/java/com/mier/mondeo/MainActivity.java
@@ -1,6 +1,5 @@
 package com.mier.mondeo;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,124 +8,77 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.material.snackbar.Snackbar;
+import com.mier.mondeo.ui.Util;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    // timer status
+    private static final byte STOPPED = 0;
+    private static final byte RUNNING = 1;
+    private static final byte PAUSED = 2;
+
     // logic variables
-    private boolean running;
     private long seconds;
     private long partialSeconds;
+    private byte status;
 
     // ui elements
-    private Button mainButton;
-    private Button resetButton;
+    private Button leftButton;
+    private Button rightButton;
     private TextView partial;
     private TextView total;
 
     // other variables
     private static final int SPLIT_ON_SCREEN_CYCLES = 5;
-    private int splitOnScreenCycles;
+    private int partialSuspendCycles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(savedInstanceState != null) {
-            running = savedInstanceState.getBoolean("running");
-            seconds = savedInstanceState.getLong("seconds");
-            partialSeconds = savedInstanceState.getLong("partialSeconds");
-            if(running) runTimer();
-        }
-
-        mainButton = findViewById(R.id.buttonStart);
-        resetButton = findViewById(R.id.buttonStop);
+        leftButton = findViewById(R.id.leftButton);
+        rightButton = findViewById(R.id.rightButton);
         partial = findViewById(R.id.currentTime);
         total = findViewById(R.id.totalTime);
 
-        splitOnScreenCycles = 0;
-        seconds = 0;
-        partialSeconds = 0;
-        running = false;
-
-        resetButton.setEnabled(false);
+        partialSuspendCycles = 0;
+        setStatus(STOPPED);
     }
 
+    /* --- status methods --- */
+
     public void onClickLeft(View view) {
-        if(!running) onClickStart();
-        else onClickLap();
+        if(status == RUNNING) partialSuspendCycles = SPLIT_ON_SCREEN_CYCLES;
+        else setStatus(RUNNING);
     }
 
     public void onClickRight(View view) {
-        if(running) onClickStop();
-        else onClickSave();
+        if(status == RUNNING) setStatus(PAUSED);
+        else {
+            setStatus(STOPPED);
+            Util.setSnackBar(view, "Successfully saved the travel.");
+        }
     }
 
-    private void onClickLap() {
-        splitOnScreenCycles = SPLIT_ON_SCREEN_CYCLES;
-        partialSeconds = 0;
-    }
-
-    private void onClickSave() {
-        clear();
-        // TODO: save
-
-        setSnackBar(findViewById(R.id.mainLayout), "Successfully saved the travel.");
-    }
-
-    private void clear() {
-        seconds = 0;
-        partialSeconds = 0;
-        updateTimers();
-        stopSaveTransition(true);
-        resetButton.setEnabled(false);
-        mainButtonTransition("start");
-    }
-
-
-    private void onClickStart() {
-        runTimer();
-        if(resetButton.getText() == getResources().getText(R.string.stop)) seconds = 0;
-        resetButton.setEnabled(true);
-        stopSaveTransition(true);
-        mainButtonTransition("lap");
-    }
-
-    private void onClickStop() {
-        mainButtonTransition("unpause");
-        stopSaveTransition(false);
-        running = false;
-    }
+    /* --- stopwatch logic --- */
 
     private void runTimer() {
         final Handler handler = new Handler();
-        running = true;
+
         handler.post(new Runnable() {
             @Override
             public void run() {
-                if(running) {
-                    updateTimers();
-                    seconds++;
-                    partialSeconds++;
-                    handler.postDelayed(this, 1000);
-                } else {
-                    seconds--;
-                    partialSeconds--;
-                }
+                if(getStatus() != RUNNING) return;
+
+                updateTimers();
+                seconds++;
+                partialSeconds++;
+                handler.postDelayed(this, 999);
             }
         });
-    }
-
-    @NonNull
-    public static int[] formatSeconds(long seconds) {
-        return new int[] {
-                (int) seconds / 3600,
-                ((int) seconds % 3600) / 60,
-                (int) seconds % 60
-        };
     }
 
     private void updateTimers() {
@@ -134,27 +86,52 @@ public class MainActivity extends AppCompatActivity {
         int[] currentTime = formatSeconds(partialSeconds);
 
         total.setText(String.format(Locale.getDefault(), "%02d:%02d:%02d", totalTime[0], totalTime[1], totalTime[2]));
-        if(splitOnScreenCycles == 0) partial.setText(String.format(Locale.getDefault(), "%02d:%02d:%02d", currentTime[0], currentTime[1], currentTime[2]));
-        else splitOnScreenCycles--;
-    }
-    
-    private void stopSaveTransition(boolean stop) {
-        resetButton.setText(getResources().getString(stop ? R.string.stop : R.string.save));
-        resetButton.setBackgroundColor(getResources().getColor(stop ? R.color.red : R.color.green, getTheme()));
+        if(partialSuspendCycles == 0 || status != RUNNING) partial.setText(String.format(Locale.getDefault(), "%02d:%02d:%02d", currentTime[0], currentTime[1], currentTime[2]));
+        else partialSuspendCycles--;
     }
 
-    private void mainButtonTransition(String mode) {
-        // append variable mode to R.string and find in resources
-        mainButton.setText(getResources().getString(getResources().getIdentifier(mode, "string", getPackageName())));
+    public static int[] formatSeconds(long seconds) {
+        return new int[] {
+            (int) (seconds / 3600),
+            (int) (seconds % 3600 / 60),
+            (int) (seconds % 60)
+        };
     }
 
-    /**
-     * Set a snackbar to the bottom of the screen
-     * @param root the root view of the activity
-     * @param snackTitle the title of the snackbar
-     */
-    public static void setSnackBar(View root, String snackTitle) {
-        Snackbar snackbar = Snackbar.make(root, snackTitle, Snackbar.LENGTH_SHORT);
-        snackbar.show();
+    /* --- buttons update --- */
+
+    private void setStatus(byte status) {
+        this.status = status;
+        rightButton.setEnabled(status != STOPPED);
+
+        int leftId;
+        int rightId = status == PAUSED ? R.string.save : R.string.stop;
+        int rightColor = status == PAUSED ? R.color.green : R.color.red;
+
+        switch(status) {
+            case RUNNING:
+                leftId = R.string.lap;
+                runTimer();
+                break;
+            case PAUSED:
+                leftId = R.string.unpause;
+                break;
+            case STOPPED:
+                leftId = R.string.start;
+                seconds = 0;
+                partialSeconds = 0;
+                updateTimers();
+                break;
+            default:
+                throw new UnsupportedOperationException("Invalid status");
+        }
+
+        leftButton.setText(getResources().getString(leftId));
+        rightButton.setText(getResources().getString(rightId));
+        rightButton.setBackgroundColor(getResources().getColor(rightColor, getTheme()));
+    }
+
+    public byte getStatus() {
+        return status;
     }
 }
---
+++ app/src/main/java/com/mier/mondeo/ui/Util.java
@@ -0,0 +1,17 @@
+package com.mier.mondeo.ui;
+
+import android.view.View;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class Util {
+    /**
+     * Set a snackbar to the bottom of the screen
+     * @param root the root view of the activity
+     * @param snackTitle the title of the snackbar
+     */
+    public static void setSnackBar(View root, String snackTitle) {
+        Snackbar snackbar = Snackbar.make(root, snackTitle, Snackbar.LENGTH_SHORT);
+        snackbar.show();
+    }
+}
